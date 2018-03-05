258dd5ef78ca78b2bb92ceb788f6a169e1526904
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-03-05 15:57:11 +0000

    params

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/test' do
   'is this working'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
